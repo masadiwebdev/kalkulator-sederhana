b31b220a724b6c013b19105803c9b780f988f0be
Arnadi WD <110708391+masadiwebdev@users.noreply.github.com>
2022-08-18 01:20:57 +0700

    Create kalkulator.php

---
+++ kalkulator.php
@@ -0,0 +1,324 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Kalkulator Sederhana</title>
+<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no"/>
+<meta name="theme-color" content="black"/>
+<!-- mulai style css -->
+<style>
+/* style untuk mereset seluruh box, margin, dan padding */
+* {
+	box-sizing: border-box;
+	margin: 0;
+	padding: 0;
+}
+/* style untuk elemen body */
+body {
+	background-color: rgba(0,0,30,.15);
+	min-height: 100vh;
+	position: relative;
+	width: 100%;
+}
+/* style untuk elemen form */
+form {
+	background-color: rgba(0,0,30,.65);
+	border: 2px outset #666;
+	border-radius: 2px;
+	box-shadow: 0 0 0 1px rgba(0,0,0), 1px 3px 3px rgba(0,0,0,.5), 2px 3px 10px 1px rgba(0,0,0,.35);
+	margin: 25vw auto;
+	max-width: 480px;
+	padding: 10px;
+	position: relative;
+	width: 90%;
+}
+/* style untuk input display atau layar utama */
+input#layar {
+	background-color: white;
+	border: 0;
+	border-radius: 1px;
+	box-shadow: 0 0 0 1px rgba(222,222,222);
+	display: block;
+	font-family: times new roman;
+	font-size: 2em;
+	font-weight: 500;
+	height: 50px;
+	line-height: 50px;
+	outline-style: none;
+	padding: 0 10px;
+	text-align: right;
+	width: 100%;
+}
+/* style untuk elemen div pembatas bari tombol */
+.row {
+	display: flex;
+	margin: 1px 0;
+	padding: 1px 5px;
+	position: relative;
+	text-align: center;
+}
+/* style untuk area atau bagian atas kalkulator */
+.row.icon {
+	display: inline-block;
+	padding: 0;
+	width: 100%;
+}
+/* style untuk area teks pojok kiri atas kalkulator */
+.row.icon span.by {
+	background-color: rgba(0,0,30,.25);
+	border: 1px inset #666;
+	border-radius: 3px;
+	color: rgba(222,222,222,.5);
+	float: left;
+	font-family: sans-serif;
+	font-size: .5em;
+	padding: 2px 5px;
+	text-align: left;
+}
+/* style untuk bagian efek kaca dibagian pojok kanan atas kalkulator */
+.row.icon span.lyr {
+	background-image: linear-gradient(25deg, rgba(222,222,222,.25), rgba(222,222,222,.45), rgba(222,222,222,.25));
+	border: 1px inset #666;
+	border-radius: 3px;
+	float: right;
+	height: 25px;
+	width: 100px;
+}
+/* style untuk jam yang ada dibagian tengah atas kalkulator */
+.row.icon span.jam {
+	background-color: rgba(0,0,30,.5);
+	border: 1px inset #666;
+	border-radius: 3px;
+	color: rgba(255,240,0,.75);
+	float: right;
+	font-family: sans-serif;
+	font-size: .8em;
+	height: 25px;
+	letter-spacing: .045em;
+	line-height: 23px;
+	margin-right: 5px;
+	width: 80px;
+}
+/* style untuk bagian pembatas layar utama */
+.row.result {
+	background-color: rgba(0,0,30,.35);
+	border: 2px inset #666;
+	border-radius: 2px;
+	padding: 2px;
+}
+/* style untuk semua tombol */
+.row button {
+	background-color: slategray;
+	border: 0;
+	border-radius: 2px;
+	box-shadow: 0 0 0 1px rgba(0,0,0,.75), 1px 1px 3px rgba(0,0,0,.75);
+	color: rgba(222,222,222);
+	font-size: 1.25em;
+	height: 40px;
+	line-height: 40px;
+	margin: 5px 1.5%;
+	opacity: 1;
+	transform: scale(1);
+	width: 100%;
+}
+/* style untuk tombol ketika diklik dan dijalankan di javascript */
+.row button.hover {
+	animation: scale 100ms;
+}
+/* style untuk setiap tombol pertama di setiap baris */
+.row button:nth-child(1) {
+	margin-left: 0;
+}
+/* style untuk setiap tombol terakhir di setiap baris */
+.row button:last-child {
+	margin-right: 0;
+}
+/* style untuk tombol del */
+.row button.del {
+	background-color: firebrick;
+}
+/* style untuk tombol c */
+.row button.c {
+	background-color: orangered;
+}
+/* style untuk semua tombol angka dan tombol titik */
+.row button.angka, .row button.titik {
+	background-color: rgba(222,222,222);
+	color: rgba(0,0,30);
+}
+/* style untuk tombol sama dengan */
+.row button.samadengan {
+	background-color: deepskyblue;
+	color: white;
+}
+/* style untuk garis pemisah bagian layar utama dengan bagian tombol */
+.line {
+	border: 2px inset #666;
+	margin: 15px 0 10px;
+}
+/* animasi yang akan dijalankan ketika tombol diklik  */
+@keyframes scale {
+	0% {
+		box-shadow: none;
+		transform: scale(.75);
+	}
+	50% {
+		box-shadow: none;
+		transform: scale(.85);
+	}
+	100% {
+		box-shadow: 0 0 0 1px rgba(0,0,0,.75), 1px 1px 3px rgba(0,0,0,.75);
+		transform: scale(1);
+	}
+}
+</style>
+<!-- akhir style css -->
+</head>
+<body>
+<!-- mulai form html -->
+<form autocomplete="off" id="form_calc">
+  <div class="row icon">
+<!-- area teks pojok kiri atas -->
+    <span class="by">Kalkulator<br>By Atakana.Com</span>
+<!-- area efek kaca -->
+    <span class="lyr"></span>
+<!-- area jam -->
+    <span class="jam"></span>
+  </div>
+<!-- area display atau layar utama kalkulator -->
+  <div class="row result">
+    <input type="text" id="layar" value="0" readonly>
+  </div>
+  <div class="line"></div>
+  <div class="row">
+<!-- tombol del atau hapus satu persatu -->
+    <button type="button" value="Del" class="del">Del</button>
+<!-- tombol peren -->
+    <button type="button" value="%" class="persen">%</button>
+<!-- tombol atau c hapus semua -->
+    <button type="button" value="C" class="c">C</button>
+<!-- tombol bagi -->
+    <button type="button" value="÷" class="bagi">÷</button>
+  </div>
+  <div class="row">
+<!-- tombol angka 7 -->
+    <button type="button" value="7" class="angka">7</button>
+<!-- tombol angka 8 -->
+    <button type="button" value="8" class="angka">8</button>
+<!-- tombol angka 9 -->
+    <button type="button" value="9" class="angka">9</button>
+<!-- tombol kali -->
+    <button type="button" value="×" class="kali">×</button>
+  </div>
+  <div class="row">
+<!-- tombol angka 4 -->
+    <button type="button" value="4" class="angka">4</button>
+<!-- tombol angka 5 -->
+    <button type="button" value="5" class="angka">5</button>
+<!-- tombol angka 6 -->
+    <button type="button" value="6" class="angka">6</button>
+<!-- tombol kurang -->
+    <button type="button" value="-" class="kurang">-</button>
+  </div>
+  <div class="row">
+<!-- tombol angka 1 -->
+    <button type="button" value="1" class="angka">1</button>
+<!-- tombol angka 2 -->
+    <button type="button" value="2" class="angka">2</button>
+<!-- tombol angka 3 -->
+    <button type="button" value="3" class="angka">3</button>
+<!-- tombol tambah -->
+    <button type="button" value="+" class="tambah">+</button>
+  </div>
+  <div class="row">
+<!-- tombol nol 0 -->
+    <button type="button" value="0" class="angka">0</button>
+<!-- tombol nol nol 00 -->
+    <button type="button" value="00" class="angka 00">00</button>
+<!-- tombol titik -->
+    <button type="button" value="." class="titik">.</button>
+<!-- tombol sama dengan -->
+    <button type="button" value="=" class="samadengan">=</button>
+  </div>
+</form>
+<!-- akhir form html -->
+<!-- mulai script javascript -->
+<script>
+// mengecek dan memulai sebuah proses sebelum tombol-tombol kalkulator dapat difungsikan
+let poby = document.querySelector("span.by");
+poby.innerHTML = "Kalkulator<br>By Atakana.Com";
+if ( poby != null && poby.innerText != "" )
+{
+// membuat jam
+  let wtk = new Date();
+  document.querySelector(".jam").innerText = wtk.getHours() + ":" + wtk.getMinutes() + ":" + wtk.getSeconds();
+// fungsi menjalankan jam aktif
+  setInterval(setClock, 1000);
+  function setClock()
+  {
+    let waktu = new Date();
+    let jam = waktu.getHours();
+    let menit = waktu.getMinutes();
+    let detik = waktu.getSeconds();
+    document.querySelector(".jam").innerText = jam + ":" + menit + ":" + detik;
+  }
+// proses kalkulator
+// mengambil input display atau layar utama kalkulator
+  let layar = document.querySelector("#layar");
+// mengambil semua elemen tombol kalkulator
+  let tombol = document.querySelectorAll("button");
+// membuat dan menjalankan proses setiap tombol yang diklik
+  for ( let i = 0; i < tombol.length; i++ )
+  {
+// even ketika tombol diklik
+    tombol[i].onclick = function()
+    {
+// menambahkan class hover pada tombol yang diklik
+	  tombol[i].classList.add("hover");
+// menghapus class hover dari tombol yang telah diklik dalam hitungan 100 mil detik
+	  setTimeout(function()
+      {
+        tombol[i].classList.remove("hover");
+      },100);
+// mengambil isi atribut value dari tombol yang diklik
+      let value = tombol[i].value;
+// jika tombol yang diklik adalah sama dengan maka jalankan script berikut
+      if ( value == "=" )
+      {
+// jumlahkan angka yang ada di layar berdasarkan penjumlahan yang dipilih, dan replace tanda persen, tanda bagi, dan tanda kali
+        layar.value = eval(layar.value.replace('%', '/100').replace('÷', '/').replace('×', '*'));
+      }
+// jika yang diklik adalah tombol c maka jalankan script berikut
+      else if( value == "C" )
+      {
+// ganti semua yang ada di layar dengan angka 0
+        layar.value = 0;
+      }
+// jika yang diklik adalah tombol del maka jalankan script berikut
+      else if ( value == "Del" )
+      {
+// jika jumlah angka di layar kurang dari sama dengan 1 maka jalankan script berikut
+        if ( layar.value.length <= 1 )
+        {
+// ganti angka di layar dengan angka 0
+          layar.value = 0;
+        }
+// dan jika jumlah angka di layar lebih dari 1 maka jalankan script berikut
+        else
+        {
+// hapus satu angka di layar setiap kali tombol del diklik
+          layar.value = layar.value.slice(0, -1);
+        }
+      }
+// dan jika selain tombol =, tombol del, dan tombol c yang diklik maka tambahkan ke layar
+      else
+      {
+// tambahkan ke layar isi dari setiap value dari tombol yang diklik, kecuali 3 yang di atas
+        layar.value = layar.value.replace(/^0+/, '') + value;
+      }
+    }
+  }
+}
+</script>
+<!-- akhir javascript -->
+</body>
+</html>
